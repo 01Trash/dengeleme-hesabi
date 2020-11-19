e5657434ccedd555c9ea68bf9b4a2ed900789091
Erdem <57351007+ErdemCmd@users.noreply.github.com>
2020-11-19 19:09:44 +0300

    Update 1_Temel_Kavramlar_ve_Hata_Kuramı.py

--- 1_Temel_Kavramlar_ve_Hata_Kuramı.py
+++ 1_Temel_Kavramlar_ve_Hata_Kuramı.py
@@ -31,24 +31,34 @@ for l in numberSS:
    E_mm = abs((numpy.longdouble(l) - A) * 1000)
    E_mm_Numbers.append(abs(E_mm))
    E_mm += E_mm
-print("[|E|]: {0:.7f} mm".format(abs(E_mm)))
+#Döngü sonucu oluşan E_mm_Numbers sayılarının toplayalı
+toplam_E_mm_Numbers = 0
+for f in E_mm_Numbers:
+   toplam_E_mm_Numbers = toplam_E_mm_Numbers + numpy.longdouble(f)
+print("[|E|]: {0:.7f} mm".format(abs(toplam_E_mm_Numbers)))
 
 # [|EE|] sayıların kareler toplamı
 EE_mm = A
+EE_mm_2_Numbers = []
 for l in numberSS:
    EE_mm = (numpy.longdouble(l) - A) * 1000
    #print("EE:", EE_mm)
    EE_mm = abs((numpy.longdouble(l) - A) * 1000)
    EE_mm_2 = EE_mm ** 2
-   EE_mm_2 = EE_mm_2 + EE_mm ** 2
-print("[|EE|]: {0:.7f} mm".format(abs(EE_mm_2)))
+   EE_mm_2_Numbers.append(abs(EE_mm_2))
+   EE_mm_2 = EE_mm_2 + (EE_mm ** 2)
+#Döngü sonucu oluşan EE_mm_2_Numbers sayılarının toplamı
+toplam_EE_mm_2_Numbers = 0
+for g in EE_mm_2_Numbers:
+   toplam_EE_mm_2_Numbers = toplam_EE_mm_2_Numbers + numpy.longdouble(g)
+print("[|EE|]: {0:.7f} mm".format(abs(toplam_EE_mm_2_Numbers)))
 
 # Ortalama hata hesabı (m)
-m = (EE_mm_2 / len(numberSS)) ** (0.5)
+m = (toplam_EE_mm_2_Numbers / len(numberSS)) ** (0.5)
 print("Ortalama hata: ±{0:.7f} mm".format(m))
 
 # Mutlak hata hesabı (t)
-t = E_mm / len(numberSS)
+t = toplam_E_mm_Numbers / len(numberSS)
 print("Mutlak hata: ±{0:.7f} mm".format(t))
 
 # Olası hata (medyan) hesabı (r)
